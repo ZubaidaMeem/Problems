f22ee76b46ca2bc8854898fd90f1b46db175623e
ZubaidaMeem <106464709+ZubaidaMeem@users.noreply.github.com>
2024-04-05 23:21:46 +0600

    Add files via upload
    Added week4 day1 codes

---
+++ XPSC/Week4/Day 1/OR_Permutation.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+#define int long long
+#define pb push_back
+#define bp(x) __builtin_popcount(x)
+#define cy cout << "YES\n"
+#define yes cout << "Yes\n"
+#define cn cout << "NO\n"
+#define no cout << "No\n"
+#define nl "\n"
+#define all(x) x.begin(), x.end()
+#define lower(x) transform(x.begin(), x.end(), x.begin(), ::tolower)
+#define upper(x) transform(x.begin(), x.end(), x.begin(), ::toupper)
+
+int cs = 1;
+#define Case cout << "Case " << cs++ << ": "
+
+const int mod = 1e9 + 7;
+const double pi = 2 * acos(0);
+int Max = INT_MAX;
+int Min = INT_MIN;
+int Inf = 1e18;
+
+using namespace std;
+
+const int N = 10005;
+vector<int> adj[N];
+bool vis[N];
+
+void print(vector<int>&v)
+{
+    for(int i = 0; i < v.size(); i++) cout << v[i] << " ";
+    cout << nl;
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int tt = 1;
+    cin >> tt;
+    while (tt--)
+    {
+        int n;
+        cin >> n;
+        vector<int>v;
+        for(int i = 0; i < n / 2; i++)
+        {
+            v.pb(i + 1);
+            v.pb(n - i);
+        }
+        if( n % 2 != 0) v.pb(n / 2 + 1);
+        for(int i = 0; i < n; i++) cout << v[i] << " ";
+        cout << nl;
+    }
+    return 0;
+}
