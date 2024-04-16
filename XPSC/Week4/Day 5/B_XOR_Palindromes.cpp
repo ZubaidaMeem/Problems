756396773fcb337efc48facf5ff2ce3c1872e4c6
ZubaidaMeem <106464709+ZubaidaMeem@users.noreply.github.com>
2024-04-16 23:11:32 +0600

    Add files via upload
    Added week 4 day 5 codes

---
+++ XPSC/Week4/Day 5/B_XOR_Palindromes.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+#define int long long
+#define pb push_back
+#define bp(x) __builtin_popcount(x)
+#define cy cout << "YES\n"
+#define yes cout << "Yes\n"
+#define cn cout << "NO\n"
+#define no cout << "No\n"
+#define nl "\n"
+#define sp " "
+#define all(x) x.begin(), x.end()
+#define lower(x) transform(x.begin(), x.end(), x.begin(), ::tolower)
+#define upper(x) transform(x.begin(), x.end(), x.begin(), ::toupper)
+
+int cs = 1;
+#define Case cout << "Case " << cs++ << ": "
+
+const int mod = 1e9 + 7;
+const double pi = 2 * acos(0);
+int Max = INT_MAX;
+int Min = INT_MIN;
+int Inf = 1e18;
+
+using namespace std;
+
+const int N = 10005;
+vector<int> adj[N];
+bool vis[N];
+
+template<class T> void print(vector<T>&v) { for (T i : v) cout << i << " "; cout << nl; }
+template<class T> void _print(vector<T>&v) { for (T i : v) cout << i.first << " " << i.second << nl; }
+template<class T> void print(set<T>&s) { for (T i : s) cout << i << " "; cout << nl; }
+template<class T, class V> void _print(map <T, V> &mp) { for (auto i : mp) cout << i.first << " " << i.second << nl; }
+template<class T, class V> void _print(pair <T, V> p) { for (auto i : p) cout << i.first << " " << i.second << nl; }
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int tt = 1;
+    cin >> tt;
+    while (tt--)
+    {
+        int n;
+        cin >> n;
+        string s;
+        cin >> s;
+        string s1 = "";
+        int l = 0, r = n - 1, p = 0, np = 0;
+        while (l < r)
+        {
+            if(s[l] == s[r]) p += 2;
+            else np ++;
+            l++, r--;
+        }
+        for(int i = 0; i <= n; i++)
+        {
+            if(i >= np)
+            {
+                int x = i - np;
+                x = max( x & 1, x - p) - (n & 1);
+                if(x <= 0 ) s1.pb('1');
+                else s1.pb('0');
+            }
+            else s1.pb('0');
+        }
+        cout << s1 << nl;
+    }
+    return 0;
+}
